99cff29ce4a68e0de7163e3ac1f339e5d0a24b81
Nithin Reddy <nithinreddygaddam@gmail.com>
2017-04-26 23:11:27 -0700

    Initial Project Setup

---
+++ Moovel/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Moovel
+//
+//  Created by Nithin Reddy Gaddam on 4/26/17.
+//  Copyright © 2017 Nithin Reddy Gaddam. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
